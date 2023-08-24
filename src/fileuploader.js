53539ba11121ecff1a85c8d4f2ae5d82466a5185
Darshan1124 <darshanthummar9429@gmail.com>
2023-08-24 20:23:22 +0530

    Update fileuploader.js

--- src/fileuploader.js
+++ src/fileuploader.js
@@ -1,6 +1,6 @@
 // FileUploader.jsx
 import axios from 'axios';
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './fileuploader.css';
 import Navbar from './navbar';
 
@@ -43,16 +43,16 @@ function FileUploader() {
             alert('Please select a file.');
             return;
         }
-    
+
         const formdata = new FormData();
         formdata.append('file', selectedfile);
-    
+
         try {
             const response = await axios.post('https://flaskend.onrender.com/convertd2p', formdata, {
                 headers: { 'Content-Type': 'multipart/form-data' },
                 responseType: 'blob', // to receive binary data
             });
-    
+
             const blob = new Blob([response.data], { type: 'application/pdf' });
             setConvertedfile(blob);
         } catch (error) {
@@ -60,7 +60,7 @@ function FileUploader() {
             console.log(error);
         }
     };
-    
+
     const handleDownload = () => {
         const url = URL.createObjectURL(convertedfile);
         const link = document.createElement('a');
@@ -73,43 +73,49 @@ function FileUploader() {
 
     return (
         <div>
-        <Navbar/>
-        <div
-            className={`converter-container ${scrolling ? 'scrolling' : ''}`}
-            onDrop={handleDrop}
-            onDragOver={(event) => event.preventDefault()}
-        >
-            <h2 className='title'
-            // className={`converter-title ${scrolling ? 'scrolling' : ''}`}
-            >WORD to PDF converter</h2>
-            <div className="drop-area">
-                {selectedfile ? (
-                    <p>Selected File: {selectedfile.name}</p>
-                ) : (
-                    <p>Drag and drop a WORD file here, or click to select a file.</p>
-                )}
-            </div>
-            {/* <input type="file" accept=".pdf" onChange={handlefilechange} className='file-input'/> */}
-            <button className="converter-button" onClick={() => document.querySelector('input[type=file]').click()}>
-                Select File
-            </button>
-            {selectedfile && (
-                <div>
-                    <button className="converter-button" onClick={converttoword}>
-                        Convert to Word
-                    </button>
+            <Navbar />
+            <div
+                className={`converter-container ${scrolling ? 'scrolling' : ''}`}
+                onDrop={handleDrop}
+                onDragOver={(event) => event.preventDefault()}
+            >
+                <h2 className='title'
+                // className={`converter-title ${scrolling ? 'scrolling' : ''}`}
+                >WORD to PDF converter</h2>
+                <div className="drop-area">
+                    {selectedfile ? (
+                        <p>Selected File: {selectedfile.name}</p>
+                    ) : (
+                        <p>Drag and drop a WORD file here, or click to select a file.</p>
+                    )}
                 </div>
-            )}
-            {convertedfile && (
-                <div className="converted-file">
-                    <h3>Converted Word file</h3>
-                    {/* <a className="download-link" href={convertedfile} download="convertedfile.docx">
+                <input
+                    type="file"
+                    accept=".pdf"
+                    onChange={handlefilechange}
+                    className="file-input"
+                    style={{ display: 'none' }}
+                />
+                <button className="converter-button" onClick={() => document.querySelector('.file-input').click()}>
+                    Select File
+                </button>
+                {selectedfile && (
+                    <div>
+                        <button className="converter-button" onClick={converttoword}>
+                            Convert to Word
+                        </button>
+                    </div>
+                )}
+                {convertedfile && (
+                    <div className="converted-file">
+                        <h3>Converted Word file</h3>
+                        {/* <a className="download-link" href={convertedfile} download="convertedfile.docx">
                         Download
                     </a> */}
-                    <button onClick={handleDownload} className="download-link">Download</button>
-                </div>
-            )}
-        </div>
+                        <button onClick={handleDownload} className="download-link">Download</button>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
